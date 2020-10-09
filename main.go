998015709dd31320e7490dce214a4b3da826af01
babyplug <poramin.le.58@ubu.ac.th>
2020-10-09 10:48:02 +0700

    Structure -- split to sub package

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/babyplug/leetcode/candies"
+)
+
+func main() {
+	// fmt.Println("Running 1d Sum")
+	// running_sum.Solved()
+
+	fmt.Println("Kids With the Greatest Number of candies")
+	candies.Solved()
+}
