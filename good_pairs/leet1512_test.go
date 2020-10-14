22c0da2466d49f3d5fbb1dfeac9b61733fe9c9b0
babyplug <poramin.le.58@ubu.ac.th>
2020-10-14 08:50:53 +0700

    Teting -- add Testing to GoodPairs

--- good_pairs/leet1512_test.go
+++ good_pairs/leet1512_test.go
@@ -2,16 +2,18 @@ package good_pairs
 
 import "testing"
 
-func TestPair(t *testing.T) {
+func TestPairShouldDisplay4(t *testing.T) {
 	result := numIdenticalPairs([]int{1, 2, 3, 1, 1, 3})
 	expected := 4
 	if result != expected {
 		t.Errorf("numIdenticalPairs(nums []int) result: got %v but expect %v", result, expected)
 	}
+}
 
-	result2 := numIdenticalPairs([]int{1, 2, 3, 1, 1, 3, 3})
-	expected2 := 6
-	if result2 != expected2 {
-		t.Errorf("numIdenticalPairs(nums []int) result: got %v but expect %v", result2, expected2)
+func TestPairShouldDisplay6(t *testing.T) {
+	result := numIdenticalPairs([]int{1, 2, 3, 1, 1, 3, 3})
+	expect := 6
+	if result != expect {
+		t.Errorf("numIdenticalPairs(nums []int) result: got %v but expect %v", result, expect)
 	}
 }
